6a8b07768f07181884ef5ee73241d490858ca0a6
yevhen.anosov <yevhen.anosov@similarweb.com>
2022-03-22 16:42:15 +0200

    tests skipped temporarily

--- tests/e2e/e2e-currency-exchange.spec.ts
+++ tests/e2e/e2e-currency-exchange.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe.only('Currency Exchange Form', () => {
+test.skip('Currency Exchange Form', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://zero.webappsecurity.com/index.html')
     await page.click('#signin_button')
