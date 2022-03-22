1a8b4837e7e2909100bc37f9b7272a452bfa50cf
yevhen.anosov <yevhen.anosov@similarweb.com>
2022-03-22 16:34:10 +0200

    payment & currency exchange

---
+++ tests/e2e/e2e-currency-exchange.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test'
+
+test.describe.only('Currency Exchange Form', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.type('#user_login', 'username')
+    await page.type('#user_password', 'password')
+    await page.click('text=Sign in')
+  })
+
+  test('Should make currency exchange', async ({ page }) => {
+    await page.click('#pay_bills_tab')
+    await page.click('text=Purchase Foreign Currency')
+    await page.selectOption('#pc_currency', 'EUR')
+
+    const rate = await page.locator('#sp_sell_rate')
+    await expect(rate).toContainText('1 euro (EUR)')
+
+    await page.type('#pc_amount', '1000')
+    await page.click('#pc_inDollars_true')
+    await page.click('#pc_calculate_costs')
+
+    const conversionAmount = await page.locator('#pc_conversion_amount')
+    await expect(conversionAmount).toContainText('1000.00 U.S. dollar (USD)')
+
+    await page.click('#purchase_cash')
+
+    const message = await page.locator('#alert_content')
+    await expect(message).toBeVisible()
+    await expect(message).toContainText(
+      'Foreign currency cash was successfully purchased'
+    )
+  })
+})
